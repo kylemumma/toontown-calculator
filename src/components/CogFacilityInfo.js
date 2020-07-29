a529e75835ee4834be8c5a4b562b5e5018cf9cdd
Cody <cdlarge77@gmail.com>
2020-07-28 19:38:12 -0700

    Website complete

--- src/components/CogFacilityInfo.js
+++ src/components/CogFacilityInfo.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './../stylesheets/main.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import BackButton from './BackButton.js'
+import BackButton from './BackButton.js';
+import ToonButton from './ToonButton.js';
+import SellbotIcon from './../ToontownMenu/Cog Facility Menu/Buttons/SellbotIcon.png';
+import CashbotIcon from './../ToontownMenu/Cog Facility Menu/Buttons/CashbotIcon.png';
+import LawbotIcon from './../ToontownMenu/Cog Facility Menu/Buttons/LawbotIcon.png';
+import BossbotIcon from './../ToontownMenu/Cog Facility Menu/Buttons/BossbotIcon.png';
 
 function CogFacilityInfo(props) {
     return (
@@ -12,6 +17,20 @@ function CogFacilityInfo(props) {
                 <Row className="justify-content-center">
                     <Col className="text-center">
                         <h1>Cog Facility Info</h1>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col xs='12' md='6' lg='3' className="text-center">
+                        <ToonButton url={SellbotIcon} alt="Sellbot" onClick={props.onClick}/>
+                    </Col>
+                    <Col xs='12' md='6' lg='3' className="text-center">
+                        <ToonButton url={CashbotIcon} alt="Cashbot" onClick={props.onClick}/>
+                    </Col>
+                    <Col xs='12' md='6' lg='3' className="text-center">
+                        <ToonButton url={LawbotIcon} alt="Lawbot" onClick={props.onClick}/>
+                    </Col>
+                    <Col xs='12' md='6' lg='3' className="text-center">
+                        <ToonButton url={BossbotIcon} alt="Bossbot" onClick={props.onClick}/>
                     </Col>
                 </Row>
             </Container>
